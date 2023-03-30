a0538fd4c640ae7f03670231e5e6478956199689
tinotringali <tringali.tino94@gmail.com>
2023-03-31 00:51:55 +0200

    agg bandiere

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,17 +1,32 @@
 <script>
-    export default {
-        name:'AppCards',
-        props:{
-            info: Object
-        }
-    }
+import CountryFlag from 'vue-country-flag-next'
+export default {
+    name: 'AppCards',
+    components: {
+        CountryFlag
+    },
+    props: {
+        info: Object
+    },
+    methods: {
+        getLanguage() {
+            switch (this.info.original_language) {
+                case "en":
+                    return "gb";
+                default:
+                    return this.info.original_language;
+            }
+        },
+
+    },
+}
 </script>
 
 <template>
     <article>
-        <h3>{{info.title}}</h3>
-        <h4>{{info.original_title}}</h4>
-        <div>{{info.original_language}}</div>
-        <div>{{info.vote_average}}</div>
+        <h3>{{ info.title }}</h3>
+        <h4>{{ info.original_title }}</h4>
+        <div><country-flag :country=getLanguage() size='small'/></div>
+        <div>{{ info.vote_average }}</div>
     </article>
 </template>
